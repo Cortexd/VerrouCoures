c468bbdafe54e49c2c60b184dcc62c24979751b7
cortexd@hotmail.com <cortexd@hotmail.com>
2018-07-02 14:01:14 +0200

    Test long press Test Crypto Debut affichage statut portail

--- app/src/main/java/com/coures/renaud/verroucoures/VerrouActivity.java
+++ app/src/main/java/com/coures/renaud/verroucoures/VerrouActivity.java
@@ -3,22 +3,35 @@ package com.coures.renaud.verroucoures;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.nio.charset.Charset;
 
 public class VerrouActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_verrou);
 
         //https://stackoverflow.com/questions/45406996/how-to-encrypt-string-in-java-and-decrypt-in-python
-        String plaintext = "Hello World!";
-        System.out.println("Plain text: " + plaintext);
+        //String plaintext = "Hello World!";
+        //System.out.println("Plain text: " + plaintext);
         //String ciphertext = new Encryption().encrypt(plaintext.getBytes("UTF-8"));
-        String ciphertext = new Encryption().encrypt(plaintext.getBytes());
-        System.out.println("Encrypted text: " + ciphertext);
+        //String ciphertext = new Encryption().encrypt(plaintext.getBytes());
+        //System.out.println("Encrypted text: " + ciphertext);
+
+        Button  button = (Button) findViewById(R.id.buttonExterieurLong);
+        button.setOnLongClickListener(new View.OnLongClickListener()
+        {
+            public boolean onLongClick(View arg0) {
+                Toast.makeText(getApplicationContext(), "Long Clicked " ,    Toast.LENGTH_SHORT).show();
+                return true;    // <- set to true
+            }
+        });
     }
 
     public void onClickPortailExterieur(View v){
@@ -32,5 +45,13 @@ public class VerrouActivity extends AppCompatActivity {
 
     public void onClickPortailIonic(View v){
         new ServiceClient().execute(new ParamRelays("IMP",7));
+    }
+
+    public void onClickGetEtatPortails(View v){
+
+        TextView tvEtatPortail = (TextView) findViewById(R.id.textViewEtatPortails);
+
+        tvEtatPortail.setText("MIEV : Ouvert\n IONIC : Fermé\n Exterieur : Fermé");
+
     }
 }
